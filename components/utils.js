7c1cd772c672572a80d372986a02b605a20ef963
John <johnrubio93@gmail.com>
2019-10-03 07:50:24 +1000

    redeploy

--- components/utils.js
+++ components/utils.js
@@ -1,6 +1,7 @@
 // Grabs the film query id off its URL property
 export const sliceFilmId = urlString => urlString.slice(-2, -1);
 
+// Functions to store and remove film id to and from local storage
 export const favClick = id => {
   const storedFavs = JSON.parse(localStorage.getItem("favs")) || [];
   storedFavs.push(id);
