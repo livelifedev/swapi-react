505c74ad9358bd844b5c3f10395c144f91e15c48
John <johnrubio93@gmail.com>
2019-10-02 17:14:26 +1000

    added tooltip

--- components/characters.js
+++ components/characters.js
@@ -5,9 +5,36 @@ const Characters = ({ charList }) => {
       <h3>Characters:</h3>
       <ul>
         {charList.map(item => {
-          return <li key={item.name}>{item.name}</li>;
+          return (
+            <li key={item.name}>
+              <span className="tooltip">
+                {item.name}
+                <span className="tooltiptext">Tooltip text</span>
+              </span>
+            </li>
+          );
         })}
       </ul>
+      <style jsx>{`
+        .tooltip {
+          position: relative;
+          display: inline-block;
+        }
+        .tooltip .tooltiptext {
+          visibility: hidden;
+          min-width: 120px;
+          background-color: black;
+          color: #fff;
+          text-align: center;
+          padding: 5px 0;
+          margin: -18px 0 0 -4px;
+          position: absolute;
+          z-index: 1;
+        }
+        .tooltip:hover .tooltiptext {
+          visibility: visible;
+        }
+      `}</style>
     </>
   );
 };
