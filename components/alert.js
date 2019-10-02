5c5e9c246f6425b1d723eea63d816bb5a47d5163
John <johnrubio93@gmail.com>
2019-10-03 00:03:21 +1000

    fixed alert placement

--- components/alert.js
+++ components/alert.js
@@ -2,12 +2,18 @@ const Alert = ({ showAlert, handleClose }) => {
   return showAlert ? (
     <div className="alert">
       <div onClick={handleClose}>
-        <p>This film in now Favourited</p>
+        <p>This film in now in your Favourites!</p>
       </div>
       <style jsx>{`
         width: 100%;
         text-align: center;
-        background-color: aquamarine;
+        background-color: #0652dd;
+        border-bottom: 1px solid #ffffff;
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 35px;
+        color: white;
       `}</style>
     </div>
   ) : null;
